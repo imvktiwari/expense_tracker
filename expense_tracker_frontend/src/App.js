a24d4cd49e7ba3b307f41447a22002e3237809b2
imvktiwari <imvktiwari2003@gmail.com>
2023-05-15 14:05:44 +0530

    fifth commit

--- expense_tracker_frontend/src/App.js
+++ expense_tracker_frontend/src/App.js
@@ -1,21 +1,47 @@
-import React, { useState } from "react";
-import Expenses from "./component/Expenses/Expenses";
-import NewExpense from "./component/NewExpense/NewExpense";
-const dummyExpenses = [
+import React from 'react';
 
-];
-function App() {
-  const [expenses, setExpenses] = useState(dummyExpenses);
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
+
+const App = () => {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   const addExpenseHandler = expense => {
-    // setExpenses([expense, ...expenses]);This can be also used to do that.
-    setExpenses(prevExpenses => {
-      return [expense, ...prevExpenses];
-    })
-  }
+    console.log('In App.js');
+    console.log(expense);
+  };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
     <div>
-      <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
-      <Expenses expenses={expenses}></Expenses>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
 }
